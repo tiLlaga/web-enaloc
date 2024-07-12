9a94dead2c0fec148019590db4b8c83f487f069d
tiLlaga <tieduardollaga@gmail.com>
2024-07-12 12:41:57 -0600

    version titulacion2024

--- src/navigation.js
+++ src/navigation.js
@@ -4,133 +4,25 @@ export const headerDataTitulacion = {
   links: [
     {
       text: 'Inicio',
-      href: '/titulacion2024',
+      href: '#',
     },
     {
       text: 'Programa de Titulación',
-      href: '/programaTitulacion',
+      href: '#programaTitulacion',
     },
     {
       text: 'Beneficios',
-      href: '/beneficios',
+      href: '#beneficios',
     },
     {
       text: 'Contáctanos',
-      href: '/contactanosLicenciatura',
+      href: '#formTitulacion',
     },
   ],
 };
 
 export const headerData = {
   links: [
-    /* {
-      text: 'Homes',
-      links: [
-        {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
-        },
-        {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
-      ],
-    },
-    {
-      text: 'Pages',
-      links: [
-        {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
-        },
-        {
-          text: 'Services',
-          href: getPermalink('/services'),
-        },
-        {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
-      ],
-    },
-    {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
-    },
-    {
-      text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
-    }, */
     {
       text: '¿Quiénes somos?',
       href: '/#quiensomos',
@@ -183,6 +75,94 @@ export const headerData = {
     },
   ],
   // actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
+};
+
+export const footerDataTitulacion = {
+  links: [
+    {
+      links: [
+        {
+          text: 'Inicio',
+          href: '/#',
+        },
+        {
+          text: 'Beneficios',
+          href: '/#',
+        },
+      ],
+    },
+    {
+      links: [
+        {
+          text: 'Programa de Titulación',
+          href: '/#',
+        },
+        {
+          text: 'Contáctanos',
+          href: '/#',
+        },
+      ],
+    },
+    /* {
+      title: 'Product',
+      links: [
+        { text: 'Features', href: '#' },
+        { text: 'Security', href: '#' },
+        { text: 'Team', href: '#' },
+        { text: 'Enterprise', href: '#' },
+        { text: 'Customer stories', href: '#' },
+        { text: 'Pricing', href: '#' },
+        { text: 'Resources', href: '#' },
+      ],
+    },
+    {
+      title: 'Platform',
+      links: [
+        { text: 'Developer API', href: '#' },
+        { text: 'Partners', href: '#' },
+        { text: 'Atom', href: '#' },
+        { text: 'Electron', href: '#' },
+        { text: 'AstroWind Desktop', href: '#' },
+      ],
+    },
+    {
+      title: 'Support',
+      links: [
+        { text: 'Docs', href: '#' },
+        { text: 'Community Forum', href: '#' },
+        { text: 'Professional Services', href: '#' },
+        { text: 'Skills', href: '#' },
+        { text: 'Status', href: '#' },
+      ],
+    },
+    {
+      title: 'Company',
+      links: [
+        { text: 'About', href: '#' },
+        { text: 'Blog', href: '#' },
+        { text: 'Careers', href: '#' },
+        { text: 'Press', href: '#' },
+        { text: 'Inclusion', href: '#' },
+        { text: 'Social Impact', href: '#' },
+        { text: 'Shop', href: '#' },
+      ],
+    }, */
+  ],
+  /* secondaryLinks: [
+    { text: 'Terms', href: getPermalink('/terms') },
+    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+  ], */
+  socialLinks: [
+    { ariaLabel: 'Tik Tok', icon: 'tabler:brand-tiktok', href: 'https://www.tiktok.com/@enalocmx' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/enalocmx/' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://es-la.facebook.com/enalocmx/' },
+    { ariaLabel: 'Youtube', icon: 'tabler:brand-youtube', href: 'https://www.youtube.com/c/EnalocM%C3%A9xico' },
+    // { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+    // { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+  ],
+  footNote: `
+    Dirección: · Teléfono: · Email: .
+  `,
 };
 
 export const footerData = {
